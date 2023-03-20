e5695e3e05ef59260dac5e15e745356f3965d755
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-03-20 14:36:24 -0700

    correct db reading, page updates on save

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const uuid = require("./helpers/uuid");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const readFromFile = util.promisify(fs.readFile);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +17,9 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/api/notes", (req, res) => res.json(db));
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note: ${req.body}`);
